62f12997fb044f471e48f1a81f0c63ade269fad5
oovwall <oovwall@163.com>
2022-11-14 07:40:16 +0800

    work time commit

--- src/components/Stock.js
+++ src/components/Stock.js
@@ -1,4 +1,4 @@
-import { Col, Row, Table, Button, Form, Input } from 'antd';
+import { Col, Row, Statistic, Divider, Table, Space, Button, Form, Input } from 'antd';
 import React, { useEffect, useState } from 'react'
 import _ from 'lodash'
 const layout = {
@@ -18,40 +18,71 @@ const tailLayout = {
 const Stock = () => {
   const [form] = Form.useForm();
 
+  /**
+   * 删除股票信息
+   * @param record
+   * @param index
+   * @param stockStoreList
+   * @param setStockStoreList
+   */
+  const deleteStock = (record, index, stockStoreList, setStockStoreList) => {
+    setStockStoreList(stockStoreList.filter(item => item.key !== record.key))
+  }
+
+  // 股票储存记录
+  const [stockStoreList, setStockStoreList] = useState([])
+  // 最新的key
+  const [stockStoreKey, setStockStoreKey] = useState(0)
+  // 平均价格
+  const [avaPrice, setAvaPrice] = useState(0)
+  // 总数
+  const [amount, setAmount] = useState(0)
+
   // 股票表列
   const stockColumns = [
     {
+      title: 'Key',
+      dataIndex: 'key',
+      width: 20
+    },
+    {
       title: '股票名称',
-      key: 'stockName',
       dataIndex: 'stockName'
     },
     {
       title: '价格',
-      key: 'price',
-      dataIndex: 'price'
+      dataIndex: 'price',
+      sorter: (a, b) => a.price - b.price,
     },
     {
       title: '数量',
-      key: 'quantity',
       dataIndex: 'quantity'
+    },
+    {
+      title: '操作',
+      key: 'operation',
+      width: 80,
+      render: (text, record, index) => (
+        <Space size="middle">
+          <a onClick={ event => deleteStock(record, index, stockStoreList, setStockStoreList) }>删除</a>
+        </Space>
+      )
     }
   ]
-  // 股票储存记录
-  const [stockStoreList, setStockStoreList] = useState([])
-  const [stockStoreKey, setStockStoreKey] = useState(1)
 
-  // useEffect(() => {
-  //   setStockStoreKey((_.last(stockStoreList)?.key || stockStoreKey) + 1)
-  //   console.log('stockStoreList', stockStoreList)
-  // })
+  useEffect(() => {
+    // 设置最新key
+    setStockStoreKey((_.last(stockStoreList)?.key || stockStoreKey) + 1)
+
+    // TODO
+    // const stockListLen = stockStoreList.length
+    // setAvaPrice()
+  }, [stockStoreList])
 
   const onFinish = (values) => {
     setStockStoreList([...stockStoreList, Object.assign(values, {
       key: stockStoreKey
     })])
-
-    console.log('stockStoreList', stockStoreList)
-    setStockStoreKey((_.last(stockStoreList)?.key || stockStoreKey) + 1)
 
     form.resetFields();
   };
@@ -63,6 +94,18 @@ const Stock = () => {
     <>
       <Row>
         <Col span={12}>
+          <Row gutter={16}>
+            <Col span={12}>
+              <Statistic title="平均价格(CNY)" value={avaPrice} precision={2} />
+            </Col>
+            <Col span={12}>
+              <Statistic title="总数" value={amount} />
+            </Col>
+          </Row>
+          <Divider plain>
+            明细
+          </Divider>
+
           <Table columns={stockColumns} dataSource={stockStoreList} />
         </Col>
         <Col span={12}>
